d210479922d0d5499a3faf6000893d99641a19d6
Chan Nyein Aung <thesmartgooners1@gmail.com>
2026-08-12 03:52:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -349,7 +349,7 @@ function initializeDefaultSettings() {
         matrixText: 'HAPPYBIRTHDAY',
         matrixColor1: '#ff69b4', // Màu pink mẫu
         matrixColor2: '#ff1493', // Màu pink mẫu
-        sequence: 'HAPPY|BIRTHDAY|TO|ANITA|❤',
+        sequence: 'HAPPY|BIRTHDAY|TO|ZIN ZIN ZAW WAI|❤',
         sequenceColor: '#ff69b4', // Màu pink mẫu
         gift: './image/happy3.gif',
         enableBook: true,
